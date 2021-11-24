144ebe5e0c06791d9d8a762b40df5c79cd29c6b9
martha <partutamarta@gmail.com>
2021-11-24 20:51:46 +0200

    refactoring and add resourses

---
+++ src/main/resources/db/migration/V2__CreateAdmin.sql
@@ -0,0 +1,5 @@
+insert into user (user_id, first_name, last_name, email, password, active)
+values (1, 'Приймальна комісія', 'Адмін', 'valdusjava@gmail.com', '123456', true);
+
+insert into access_level (user_id, access_levels)
+values (1, 'ADMIN');
